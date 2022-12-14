bd30d52b53598b031e4c1064c405dea7b7f7a5ea
sofiarondini <119893949+sofiarondini@users.noreply.github.com>
2022-12-14 16:54:36 -0300

    final

---
+++ 5) blank LinesCounter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function 5blankLinesCounter {
+  [ ! -f $1 ] && echo "Archivo inexistente, intente nuevamente" && exit 1
+
+
+   total_lineas=$(cat $1 | wc -l)
+   lineas_con_contenido=$(cat  $1 | grep . | wc -l)
+
+   total_lineas_blanco=$((total_lineas-lineas_con_contenido))
+
+   echo La cantidad de líneas en blanco es $total_lineas_blanco
+}
+
+#grep . matchea todas las líneas que contengan algún caracter
