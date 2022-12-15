dc373323f4fb6cd28f77a7289a34630e6fd99ac4
sofiarondini <119893949+sofiarondini@users.noreply.github.com>
2022-12-15 20:36:20 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-source 1statsWords.sh
-source 2statsUsageWords.sh
-source 3findNames.sh
-source 4statsSentences.sh
-source 5blankLinesCounter.sh
-
 PATH_ARCHIVO=texto.txt
 
 PS3='=> '
 echo "Elegì una opcion"
 select opcion in "SALIR" "Ealabras" "uso_de_palabras" "nombres" "oraciones" "lineas_en_blanco"
 do
-	[ -z $opcion ] && echo "Eleg una opcion valida" && continue
+	[ -z $opcion ] && echo "Elegì una opcion valida" && continue
 	[ $REPLY == 1 ] && echo "Saliendo.." && break
 	echo "Opcion elegida: " $opcion
 
 	case $REPLY in
-	  2) 1statsWords $PATH_ARCHIVO;;
-	  3) 2statsUsageWords $PATH_ARCHIVO;;
-	  4) 3findNames $PATH_ARCHIVO;;
-	  5) 4statsSentences $PATH_ARCHIVO;;
-	  6) 5blankLinesCounter $PATH_ARCHIVO;;
+	  2) ./1statsWords.sh $PATH_ARCHIVO;;
+	  3) ./2statsUsageWords.sh $PATH_ARCHIVO;;
+	  4) ./3findNames.sh $PATH_ARCHIVO;;
+	  5) ./4statsSentences.sh $PATH_ARCHIVO;;
+	  6) ./5blankLinesCounter.sh $PATH_ARCHIVO;;
 	esac
   
 done
